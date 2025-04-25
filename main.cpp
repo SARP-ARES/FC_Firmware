10dc050dfe1a9257288c184794502ccd716920d9
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2025-04-24 20:44:01 -0700

    started writing a main.cpp

---
+++ main.cpp
@@ -0,0 +1,76 @@
+// Testing data logging with flash chip
+#include "mbed.h"
+#include "EUSBSerial.h"
+#include "flash.h"
+#include <cstdint>
+
+
+
+
+
+int main() {
+
+    // instantiate flash chip
+    flash fc(PA_7, PA_6, PA_5, PA_4); // MOSI, MISO, SCLK, CS (Flight Computer)
+
+    // make test data
+    float lat = 90.0;
+    float lon = 180.0;
+    int pSize_float_bytes = 8; // 2 floats = 8 bytes
+
+    int packetSize = 8;
+
+    // uint32_t flashAddress = 0; // start writing at address 0
+
+    while (true){
+        // TESTING...
+        // THIS IS IN THE LOOP ON PURPOSE TO ONLY WRITE TO THE FIRST [packetSize] BYTES
+        uint32_t flashAddress = 0; // start writing at address 0
+
+        // initialize packet buffer
+        uint8_t dataLog[packetSize];
+
+        // initialize arrays to store floats as bytes
+        uint8_t lat_bytes[4];
+        uint8_t lon_bytes[4];
+
+        // turn all float data into bytes
+        float2Byte(lat_bytes, lat);
+        float2Byte(lon_bytes, lon);
+
+        // store all bytes in dataLog
+        int index = 0;
+        for (int i=0; i<4; i++){
+            dataLog[index] = lat_bytes[i];
+            index++;
+        }
+        for (int i=0; i<4; i++){
+            dataLog[index] = lon_bytes[i];
+            index++;
+        }
+
+        // write datalog and bump flashAddress to the next empty address
+        fc.write(flashAddress, dataLog, packetSize);
+        flashAddress = flashAddress + packetSize;
+
+
+        // OR I CAN JUST USE WRITENUM
+        flashAddress = fc.writeNum(flashAddress, lat); // bytes 0-3
+        flashAddress = fc.writeNum(flashAddress, lon); // bytes 4-7
+        
+
+        // fc.write(flashAddress, packet_floats, pSize_floats)
+
+        // flashAddress = flashAddress + pSize_floats // step to the address after the packet
+
+        // fc.write(packet_ints)
+        // flashAddress = flashAddress + pSize_ints
+
+        // fc.write(packet_chars)
+        // flashAddress = flashAddress + pSize_chars
+
+    }
+
+
+}
+
