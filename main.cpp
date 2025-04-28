164ea00cf5ba5b1b6d0f0323b1a708cddb5123f6
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2025-04-28 03:11:57 -0700

    testing updateFlightPacket method

--- main.cpp
+++ main.cpp
@@ -1,83 +1,61 @@
 // Testing data logging with flash chip
-#include "ThisThread.h"
+// #include "ThisThread.h"
 #include "mbed.h"
-#include "BMP280.h"
 #include "EUSBSerial.h"
+#include "ctrldRogallo.h"
+#include "GPS.h"
+#include "BMP280.h"
+#include "BNO055.h"
 #include "flash.h"
 #include "flight_packet.h"
-#include <cstdint>
+// #include <cstdint>
+
 
 int main() {
     
     // instantiate flash chip
-    flash fc(PA_7, PA_6, PA_5, PA_4); // MOSI, MISO, SCLK, CS (Flight Computer)
-
+    // Instantiates: GPS gps(PA_2, PA_3), BMP280 bmp(PB_7, PB_8, 0xEE), BNO055 bno(PB_7, PB_8, 0x51), fc(PA_7, PA_6, PA_5, PA_4)
+    // Q: should make objects in main and pass pointers instead?
+    ctrldRogallo ARES;
     EUSBSerial pc(true);
     DigitalOut led_B(PA_8);
-    led_B.write(1);
+    led_B.write(0);
 
-    FlightPacket packet;
+    // FlightPacket packet;
     int packetSize = sizeof(FlightPacket);
     uint32_t readAddress = 0;
-    pc.printf("Starting flash chip testing...");
+    ThisThread::sleep_for(2s); // wait for serial port to connect?
+    pc.printf("\nStarting flash chip testing...");
     while (true){
         // TESTING...
         // THIS IS IN THE LOOP ON PURPOSE TO ONLY WRITE TO THE FIRST [packetSize] BYTES
+        pc.printf("\n\nErasing first sector of flash chip...");
         uint32_t flashAddress = 0; // start writing at address 0
-        fc.eraseSector(flashAddress); // to not overwrite stuff
-
-
-        /* FLIGHT PACKET STRUCT FOR REFERENCE
-        struct FlightPacket {
-        uint32_t timestamp_utc;     // 4 bytes
-        uint8_t fsm_mode;           // 1
-        float heading_deg;          // 4
-        float target_heading_deg;   // 4
-        float groundspeed_m_s;      // 4
-        float v_speed_m_s;          // 4
-        float latitude_deg;         // 4
-        float longitude_deg;        // 4
-        float altitude_m;           // 4
-        float pos_east_m;           // 4
-        float pos_north_m;          // 4
-        float pos_up_m;             // 4
-        float temp_c;               // 4
-        float pressure_pa;          // 4
-        float delta1;               // 4
-        float delta_1_m;            // 4
-        float delta2;               // 4
-        float delta2_m;             // 4
-        float delta_a;              // 4
-        float delta_s;              // 4
-        float pwm_motor1;           // 4
-        float pwm_motor2;           // 4
-        float fc_cmd;               // 4
-        char id[8];                 // 8 (
-        */
-
-        // store data in the packet struct
-        packet.timestamp_utc = 0;
-        packet.fc_cmd = 0.8;
-        packet.heading_deg = 100;
-        packet.latitude_deg = 47.0;
-        packet.longitude_deg = -122.0;
-        packet.fsm_mode = 3;
-        // packet.pressure_pa = NAN;
+        ARES.fc.eraseSector(flashAddress); // to not overwrite stuff
+        
+        // get updated values from all sensors 
+        // and store them in the flight packet
+        pc.printf("\nUpdating flight packet...");
+        ARES.updateFlightPacket();
 
         // write the packet to the flash chip
-        flashAddress = fc.writePacket(flashAddress, packet);
+        pc.printf("\nWriting packet to address: %d (Packet Size: %d)", ARES.currentFlashAddress, packetSize);
+        ARES.logDataTEST();
+        pc.printf("\nData written... New flash address: %d", ARES.currentFlashAddress);
+        
 
         // initialize struct and read one packet from flash chip
         FlightPacket packet_read; // buffer
-        fc.readPacket(readAddress, packet_read);
+        ARES.fc.readPacket(readAddress, packet_read);
 
+        pc.printf("\nReading packet at address: %d", readAddress);
         pc.printf("\n==================================");
-        pc.printf("\nFSM Mode:\t%d \nLat:\t\t%f \nLon:\t\t%f \nPressure:\t%f", \
-                    packet_read.fsm_mode, packet_read.latitude_deg, packet_read.longitude_deg, packet.pressure_pa);
-        ThisThread::sleep_for(2s);
+        pc.printf("\nFSM Mode\t:%d \nFix\t\t:%d \nLat\t\t:%f \nLon\t\t:%f \nPressure\t:%f Pa \nTemperature\t:%f", \
+                    packet_read.fsm_mode, packet_read.gps_fix, packet_read.latitude_deg, packet_read.longitude_deg, packet_read.pressure_pa, packet_read.temp_c);
+        pc.printf("\n==================================");
+        ThisThread::sleep_for(5s);
+
 
     }
 
-
 }
-
