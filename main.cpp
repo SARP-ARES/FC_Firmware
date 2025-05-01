6d74caba3f289c18129055ee1c0cfb554b31a66b
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2025-05-01 01:50:12 -0700

    MCPS->FC works, FC->MCPS does not >:((

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Testing data logging with flash chip
+// Testing MCPS-FC Comms
 #include "mbed.h"
 #include "EUSBSerial.h"
 #include "I2CSerial.h"
@@ -6,26 +6,33 @@
 
 
 void flightComputer() {
-    EUSBSerial pc();
     I2CSerial master(PB_3, PB_10, 0x32, false);
+    EUSBSerial pc(false);
+    ThisThread::sleep_for(1s);
+    DigitalOut led_B(PA_8);
+    led_B.write(1);
     int ctrl[6] = {100, -100, 0, 25, 50, 100};
 
+
     while (true){
-        
         // WRITING CONTROL SEQUENCE
         ThisThread::sleep_for(1s); // other processes
 
         char buf[256] = {0};
         for (int i = 0; i<6; i++){
             // write control to MCPS
-            pc.printf("Sending: %+03d", ctrl[i]);
-            master.printf("%+03d", ctrl[i]);
-            ThisThread::sleep_for(100ms);
+            pc.printf("\nSending: %+04d\n", ctrl[i]);
+            master.printf("%+04d\n", ctrl[i]);
+            ThisThread::sleep_for(200ms);
 
+                        
             // MCPS should send it back
-            master.read(buf, 256);
-            pc.printf("Recieving: %s", buf);
-
+            if (master.readline(buf, 256)) {
+                pc.printf("Recieving: %s\n", buf);
+            } else {
+                pc.printf("Nothing Recieved...\n");
+            }
+        
             ThisThread::sleep_for(2s); // change to 600s (10min) for flight
         }
     }
@@ -35,24 +42,69 @@ void flightComputer() {
 void mcps() {
     I2CSerial slave(PB_7, PB_8, 0x32, true);
     DigitalOut led_R(PC_14);
-    led_R.write(1);
+    led_R.write(0);
 
     while (true) {
         // read FC message into buffer
         char buf[256] = {0};
         for (int i = 0; i<5; i++) { // limit max packet reads per cycle
-            slave.readline(buf, 256);
+            
+            if (!slave.readline(buf, 256)) {
+                break;
+            }
             //    .read(char* buf, size_t num_bytes); // reads num_bytes bytes into the buffer
             // handle messages here
+            slave.printf("MCPS->FC: %s\n", buf);
+            // slave.write(buf, 256);
+
         }
 
         // write the buffer back to FC
-        slave.write(buf, 256); // for fix sized writes / reads or raw byte data  
+        // for fix sized writes / reads or raw byte data  
     }
-    
 }
 
 
+void mcp_log() {
+    Timer t;
+    size_t count = 0;
+
+    I2CSerial master(PB_3, PB_10, 0x32, false);
+    EUSBSerial pc(0x3232, 0x1);
+
+    ThisThread::sleep_for(1s);
+
+    t.start();
+    while (true) {
+        char buf[256] = {0};
+        if (master.readline(buf, 256)) {
+            pc.printf("(MCP) %s", buf);
+            t.reset();
+        }
+
+        if (t.read_ms() > 5000) {
+            pc.printf("(FC) KeepAlive %d\n", count);
+            count ++;
+            t.reset();
+        }
+    }
+}
+
+void mcp() {
+    I2CSerial slave(PB_7, PB_8, 0x32, true);
+
+    DigitalOut led_R(PC_14);
+    led_R.write(1);
+
+    int c = 0;
+    while (true) {
+        ThisThread::sleep_for(1s);
+
+        slave.printf("Hello World: %d\n", c);
+        c++;
+    }
+}
+
 int main() {
-    mcps();
+    flightComputer();
 }
