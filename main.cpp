89201874636fa6d3c5144526d70fca1642e42173
LukeVerlan <verlangieriluke@gmail.com>
2025-10-10 18:24:38 -0700

    cleaned up main, removed repeated code and fixed minor memory overconsumption

--- main.cpp
+++ main.cpp
@@ -17,21 +17,26 @@ DigitalOut led_B(PA_8);
 DigitalOut led_G(PA_15);
 Thread thread;
 
+FlightPacket state; // global state makes more sense than individual logging states (could be moved to 
+                    // logging multi-function as a pointer)
+
+enum FlightMode {
+    test,
+    packetlog, 
+    ctrl_sequence
+};
+
 // = # of pages, 4.55 hours at 1Hz
 // 16 pages per sector
-uint32_t packetSize = sizeof(FlightPacket);
-uint32_t MAX_NUM_PACKETS = 16384; 
-uint32_t eraseAddr = 0;
+
+#define FLIGHT_PACKET_SIZE  sizeof(FlightPacket) // Size in bytes of one flight packet 
+#define MAX_NUM_PACKETS     16384
+
+/* SET NUMBER OF PACKETS TO LOG || for for 1.5 hours of logging, log 5400 packets */
+#define NUM_PACKETS_TO_LOG  16000
 
 
-/*
- * SET NUMBER OF PACKETS TO LOG
- * for for 1.5 hours of logging, log 5400 packets
- */
-uint32_t numPackets = 16000;
-
-
-
+/** @brief clears all data off of the flash chip */
 void clear_data() {
     led_B.write(0);
     Timer t;
@@ -45,9 +50,13 @@ void clear_data() {
     led_B.write(1);
 }
 
-
-
-void flight_log(ctrldRogallo* ARES, uint32_t numPacketLog) {
+/** 
+ * @brief logs a given amount of packets to the flash chip and stops running ARES when logging is exhausted
+ * @param ARES pointer to the ARES flight object
+ * @param numPacketLog total number of packets to log before system ends
+ * @param flash_addr pointer to the current system flash address 
+ */
+void flight_log(ctrldRogallo* ARES, uint32_t numPacketLog, uint32_t* flash_addr) {
 
     /* 
      * start ctrl_trigger high, write low once apogee is detected and fsm_mode =
@@ -56,51 +65,31 @@ void flight_log(ctrldRogallo* ARES, uint32_t numPacketLog) {
     
     DigitalOut ctrl_trigger(PB_3); 
     ctrl_trigger.write(1); 
-    
-    ThisThread::sleep_for(1s);
-    uint32_t currentFlashAddress = 0;
-
-    // --------------------------------------------------------
-
-    pc.printf("\nCollecting %d %d-byte packets at 1Hz...\n", numPacketLog, packetSize);
-    pc.printf("\nARES IS READY TO INSTALL\n");
     led_G.write(1);
-
 
     // big write
     for (int i = 0; i < numPacketLog; i++) {
 
         ARES->updateFlightPacket(); 
    
-        FlightPacket state = ARES->getState(); // extract state variables
-        currentFlashAddress = fc.writePacket(currentFlashAddress, state); // write state variables to flash chip
+        state = ARES->getState(); // extract state variables
+        *flash_addr = fc.writePacket(*flash_addr, state); // write state variables to flash chip
 
         if (state.fsm_mode == FSM_SEEKING) { // mode is set after apogee detection
             ctrl_trigger.write(0); // signal to control sequence on MCPS 
         }
     }
 
-    pc.printf("=================================%s\n", ' ');
-    pc.printf(" ARES DATA COLLECTION FINISHED %s\n", ' ');
-    pc.printf("=================================%s\n", ' ');
-
 } 
 
-
-void test_mode(ctrldRogallo* ARES){
+/** 
+ *  @brief Testing mode system runs & logs until "quit" is entered into the CLI 
+ *  @param ARES pointer to the ctrldRogallo flight object
+ *  @param flash_addr pointer to the current system flash address 
+ */
+void test_mode(ctrldRogallo* ARES, uint32_t* flash_addr){
 
     char cmdBuf[32];
-
-    ARES->updateFlightPacket();
-    ARES->setThreshold(); 
-
-    pc.printf("Beginning data collection... %s\n", ' ');
-    pc.printf("==========================================================\n");
-    
-    ThisThread::sleep_for(1s);
-    uint32_t currentFlashAddress = 0; // start writing at the first flash address
-
-    FlightPacket state;
 
     while(true) {
         // Get current data and put it in the state struct
@@ -110,8 +99,8 @@ void test_mode(ctrldRogallo* ARES){
         // Print data to serial port
         ARES->printCompactState(&pc);
 
-        // Write data to flash chip
-        currentFlashAddress = fc.writePacket(currentFlashAddress, state);
+        // Write data to flash chip & increment counter
+        *flash_addr = fc.writePacket(*flash_addr, state);
 
         // TODO: make control sequence / seeking logic 
         if (state.fsm_mode == FSM_SEEKING) { // mode is set after apogee detection
@@ -125,47 +114,33 @@ void test_mode(ctrldRogallo* ARES){
         // break on "quit" command
         if(pc.readline(cmdBuf, sizeof(cmdBuf))) {
             if(strcmp(cmdBuf, "quit") == 0) {
+                pc.printf("\"quit\" cmd recieved...\n");
                 break;
             }
         }
     }
-    
-    pc.printf("==============================%s\n", ' ');
-    pc.printf("\"quit\" cmd recieved...\n");
-    pc.printf("ARES data collection complete\n");
-    pc.printf("==============================%s\n", ' ');
 }
 
+/** 
+ * @brief starts running the control sequence after main has been deployed
+ * @param ARES pointer to the ctrldRogallo flight object
+ * @param flash_addr pointer to the current address the flash chip is ready to write 
+ */
+void ctrl_sequence_after_apogee(ctrldRogallo* ARES, uint32_t* flash_addr){
 
-void ctrl_sequence_after_apogee(ctrldRogallo* ARES){
-
-    char cmdBuf[32];
-
-    ARES->updateFlightPacket();
-    ARES->setThreshold(); 
-
-    pc.printf("Beginning control sequence... %s\n", ' ');
-    pc.printf("==========================================================\n");
-    
-    ThisThread::sleep_for(1s);
-    uint32_t currentFlashAddress = 0; // start writing at the first flash address
-
-    FlightPacket state;
+    char cmdBuf[32]; //CLI buffer
 
     while(true) {
         // Get current data and put it in the state struct
         ARES->updateFlightPacket();
         state = ARES->getState();
         
-
-        
         // Print data to serial port
         ARES->printCompactState(&pc);
 
-        // Write data to flash chip
-        currentFlashAddress = fc.writePacket(currentFlashAddress, state);
+        // Write data to flash chip increment the flash addr counter
+        *flash_addr = fc.writePacket(*flash_addr, state);
 
-        
         if (state.fsm_mode == FSM_SEEKING) { // mode is set after apogee detection
             // TODO: CONTROL SEQUENCE HERE
             // use sendCtrl() method
@@ -174,21 +149,15 @@ void ctrl_sequence_after_apogee(ctrldRogallo* ARES){
         // break on "quit" command
         if(pc.readline(cmdBuf, sizeof(cmdBuf))) {
             if(strcmp(cmdBuf, "quit") == 0) {
+                pc.printf("\"quit\" cmd recieved...\n");
                 break;
             }
         }
     }
-    
-    pc.printf("==============================%s\n", ' ');
-    pc.printf("\"quit\" cmd recieved...\n");
-    pc.printf("ARES data collection complete\n");
-    pc.printf("==============================%s\n", ' ');
 }
 
-
-
+/** @brief dumps all logged data in the serial port */
 void dump_data(){
-
     uint16_t numPacketDump;
     fc.read(0x3FFFFE, reinterpret_cast<uint8_t*> (&numPacketDump), 2);
 
@@ -204,10 +173,9 @@ void dump_data(){
         fc.dumpAllPackets(numPacketDump);
         pc.printf("==================================\n");
     }
-
 }
 
-
+/** @brief verification step for the CLI */
 void double_check() {
     char cmdBuf[32];
     pc.printf("Are you sure you want to erase all data from ARES?\n");
@@ -245,15 +213,14 @@ void double_check() {
             break;
         }
     }
-        
 }
 
-
+/** @brief Sets the relative origin for ARES where ever we are currently */
 void set_origin(ctrldRogallo* ARES) {
     // no lat/lon argument
     // sets current lat/lon as target/origin
     ARES->updateFlightPacket(); // get current lat/lon
-    FlightPacket state = ARES->getState();
+    state = ARES->getState();
 
     if (state.latitude_deg != NAN && state.longitude_deg != NAN){
         ARES->setTarget(state.latitude_deg, state.longitude_deg);
@@ -264,6 +231,11 @@ void set_origin(ctrldRogallo* ARES) {
     }
 }
 
+/** 
+ * @brief Sets the relative origin for ARES at the given coordinates
+ * @param lat lattidue in deg 
+ * @param lon longitude in deg
+ */ 
 void set_origin(ctrldRogallo* ARES, double lat, double lon) {
     // uses lat/lon arg to set target/origin
     ARES->setTarget(lat, lon);
@@ -271,6 +243,41 @@ void set_origin(ctrldRogallo* ARES, double lat, double lon) {
                 lat, lon);
 }
 
+/** 
+ * @brief Determines what flight (logging) mode to enter used for readability 
+ * @param mode the FlightMode enum to enter
+ * @param ARES reference to the ctrldRogallo flight object
+ * @param num_packets -- for limited packet logging number of packets to log
+ */
+void flight_mode(FlightMode mode, ctrldRogallo* ARES, uint32_t num_packets) {
+    ARES->setThreshold();
+    ARES->updateFlightPacket();
+    ThisThread::sleep_for(1s);
+    uint32_t flash_addr = 0; 
+    switch (mode) {
+        case packetlog:
+            flight_log(ARES, num_packets, &flash_addr);      // Packet logging
+            pc.printf("\nCollecting %d %d-byte packets at 1Hz...\n", num_packets, FLIGHT_PACKET_SIZE);
+            pc.printf("==========================================================\n");
+            break; 
+        
+        case test:  
+            test_mode(ARES, &flash_addr);                    // Testing mode
+            pc.printf("\nEntering Testing mode... %s\n", ' ');
+            pc.printf("==========================================================\n");
+            break; 
+
+        case ctrl_sequence:
+            ctrl_sequence_after_apogee(ARES, &flash_addr);   // Controlled mode
+            pc.printf("\nBeginning control sequence... %s\n", ' ');
+            pc.printf("==========================================================\n");
+            break; 
+    }
+
+    pc.printf("==============================%s\n", ' ');
+    pc.printf("ARES data collection complete\n");
+    pc.printf("==============================%s\n", ' ');
+}
 
 void command_line_interface() {
     char cmd_buffer[32];  // user input buffer
@@ -299,7 +306,7 @@ void command_line_interface() {
             if (strcmp(cmd_buffer, "test_mode") == 0 || strcmp(cmd_buffer, "1") == 0) {
                 pc.printf("\"test_mode\" cmd received. Use \"quit\" cmd to stop logging.\n");
                 ThisThread::sleep_for(1500ms);
-                test_mode(&ARES);
+                flight_mode(test,&ARES,0);
             }
 
             // dump data
@@ -365,8 +372,6 @@ void command_line_interface() {
     }
 }
 
-
 int main() {
     command_line_interface();
 }
-
